73cace2acabe82a223fb7615f784e576ba039af9
Broly <vidovicp20@gmail.com>
2022-11-09 14:40:41 -0500

    updated formatting and added delete and cascade features to schema

--- schema.sql
+++ schema.sql
@@ -4,12 +4,12 @@ CREATE DATABASE CMS;
 USE CMS;
 
 CREATE TABLE department(
-id INT PRIMARY KEY AUTO_INCREMENT, 
-name VARCHAR(30)
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+name VARCHAR(30) NOT NULL
 ); 
 
 CREATE TABLE role(
-id INT PRIMARY KEY AUTO_INCREMENT,
+id INT AUTO_INCREMENT PRIMARY KEY,
 
 title VARCHAR(30),
 
@@ -17,14 +17,16 @@ salary DECIMAL,
 
 department_id INT,
 
-FOREIGN KEY (department_id) REFERENCES department(id)
+FOREIGN KEY (department_id)
+REFERENCES department(id)
+ON DELETE SET NULL
 
 ); 
 
 
 CREATE TABLE employee(
 
-id INT PRIMARY KEY AUTO_INCREMENT,
+id INT AUTO_INCREMENT PRIMARY KEY,
 
 first_name VARCHAR(30),
 
@@ -32,9 +34,12 @@ last_name VARCHAR(30),
 
 role_id INT,
 
-FOREIGN KEY (role_id) REFERENCES role(id),
+FOREIGN KEY (role_id) 
 
-manager_id INT,
+REFERENCES role(id)
+
+ON DELETE SET NULL,
 
 FOREIGN KEY (manager_id) REFERENCES employee(id)
+ON DELETE CASCADE
 );
